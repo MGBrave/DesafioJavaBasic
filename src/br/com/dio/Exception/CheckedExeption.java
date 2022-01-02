219c68e1580a6a94039fe2ecc98a510cd8b20cd2
Mgeraldo <mggeraldo@hotmail.com>
2022-01-02 10:27:13 -0300

    A mudança desafio Dio -

---
+++ src/br/com/dio/Exception/CheckedExeption.java
@@ -0,0 +1,31 @@
+package br.com.dio.Exception;
+
+import java.io.*;
+
+//imprimir um arquivo no console
+/*public class CheckedExeption {
+    public static void main(String[] args) {
+        String nomeDoArquivo = "romances-blacke-crouch.txt";
+        imprimirArquivoNoConsole(nomeDoArquivo);
+
+        System.out.println("Apesar da exception ou não, o programa continua...");
+    }
+
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo) {
+        File file = new File(nomeDoArquivo);
+        BufferedReader br = new BufferedReader((new FileReader(file.getName());
+        String line = br.readLine();
+
+        BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            line=br.readLine();
+        } while (line!=null);
+        bw.flush();
+        bw.close();
+    }
+}
+*/
+

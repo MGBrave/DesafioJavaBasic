c2e3179583b08b4054efdb90065289fc84c136a6
Mgeraldo <mggeraldo@hotmail.com>
2022-01-02 20:25:56 -0300

    ExemploLinkedList queue java

---
+++ src/br/com/dio/queue/ExemploLinkedList.java
@@ -0,0 +1,55 @@
+package br.com.dio.queue;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ExemploLinkedList {
+    public static void main(String[] args) {
+        Queue<String> filaBanco = new LinkedList<>();
+        filaBanco.add("Flávio");
+        filaBanco.add("Patricia");
+        filaBanco.add("Roberto");
+        filaBanco.add("Pamela");
+        filaBanco.add("Anderson");
+
+        System.out.println(filaBanco);
+        String clienteASerAtendido = filaBanco.poll();
+
+        System.out.println(clienteASerAtendido);
+
+        System.out.println(filaBanco);
+        //filaBanco.clear();
+
+        String primeiroCliente = filaBanco.peek();
+
+        System.out.println(primeiroCliente);
+
+        System.out.println(filaBanco);
+
+        String primeiroClienteOuErro = filaBanco.element();
+
+        System.out.println(primeiroClienteOuErro);
+
+        System.out.println(filaBanco);
+
+        for (String client:filaBanco){
+            System.out.println(client);
+
+        }
+        Iterator<String> iteratorFilaBanco = filaBanco.iterator();
+
+        while(iteratorFilaBanco.hasNext()){
+            System.out.println("--->" + iteratorFilaBanco.next());
+        }
+        System.out.println(filaBanco.size()); //quantos elementos a fila tem
+
+        System.out.println(filaBanco.isEmpty());//se a fila está vazia
+
+        //filaBanco.clear();
+
+        filaBanco.add("Wesley");
+        System.out.println(filaBanco);
+
+    }
+}
